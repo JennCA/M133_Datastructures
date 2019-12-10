fe0a62a0d52dc4586bf04796de5517bf98d97644
JennCA <jennifer.cicvarova@gmail.com>
2019-12-10 19:31:19 +0100

    create size, isEmpty, add, peek, poll tests

--- lib/Queue.ts
+++ lib/Queue.ts
@@ -1,7 +1,26 @@
 import { IDataScructure } from "./IDataStructure";
 
 export class Queue implements IDataScructure {
+
+    private queue = [];
+
     public size(): number {
-        return undefined;
+        return this.queue.length;
+    }
+
+    public isEmpty(): boolean {
+        return (!this.size()) ? true : false;
+    }
+
+    public add(value: any): void {
+        this.queue.push(value);
+    }
+
+    public peek(): any {
+        return this.queue[0];
+    }
+
+    public poll(): any {
+        return this.queue.shift();
     }
 }
--- test/structures.tests.ts
+++ test/structures.tests.ts
@@ -3,18 +3,131 @@ import { Queue } from "../lib/Queue";
 
 describe("Data structures", () => {
     describe("Queue", () => {
-        it("has size", () => {
-            const testee = new Queue();
-            const result = testee.size();
-            expect(result).to.equal(0);
-        })
-    })
+        var queue: Queue;
 
-    // describe("Factory", () => {
-    //     it("can create Queue", () => {
-    //         const structureType = DataStructures.Queue;
-    //         const result = DataStructureFactory.create(structureType);
-    //         expect(result).to.be.an.instanceOf(Queue);
-    //     })
-    // })
+        beforeEach(() => {
+            queue = new Queue();
+        });
+
+        it("has size", () => {
+            const expected = 0
+            const result = queue.size();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('can enqueue artist and queue has size of 1', () => {
+            const artist = 'Rich Brian';
+            const expected = 1;
+
+            queue.add(artist);
+            const result = queue.size();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('can enqueue 5 artist and queue has size of 5', () => {
+          const artists = [
+              'Rich Brian',
+              'Darude',
+              'Jackson Wang',
+              'G-Dragon',
+              'Hans'
+            ];
+          const expected = 5;
+
+          artists.forEach(artist => queue.add(artist));
+          
+          const result = queue.size();
+
+          expect(result).to.equal(expected);
+        });
+
+
+        it('is empty', () => {
+            const expected = true;
+
+            const result = queue.isEmpty();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('is not empty', () => {
+            const artist = "Rich Brian";
+            const expected = false;
+
+            queue.add(artist);
+            const result = queue.isEmpty();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('can peek from empty queue', () => {
+            const expected = undefined;
+
+            const result = queue.peek();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('can peek from queue with artist', () => {
+            const artist = 'Rich Brian';
+
+            queue.add(artist);
+            const result = queue.peek();
+
+            expect(result).to.equal(artist); 
+        });
+
+        it("can peek from queue with 3 artists", () => {
+            const artists = ["G-Dragon", "Darude", "Jackson Wang", "Rich Brian", "Hans"];
+            const expected = 'G-Dragon';
+
+            artists.forEach(artist => queue.add(artist));
+            const result = queue.peek();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('can poll from empty queue', () => {
+            const expected = undefined;
+
+            const result = queue.poll();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('can poll artist from queue with 4 artists', () => {
+            const artists = ["Hans", "Darude", "Jackson Wang", "G-Dragon"];
+            const expected = 'Hans';
+
+            artists.forEach(artist => queue.add(artist));
+            const result = queue.poll();
+
+            expect(result).to.equal(expected);
+        });
+
+        it('can poll artists in correct order', () => {
+            const artists = ["Hans", "Darude", "Jackson Wang", "G-Dragon"];
+
+            artists.forEach(artist => queue.add(artist));
+            var result = [];
+
+            while (!queue.isEmpty())
+                result.push(queue.poll());
+
+            expect(result).to.deep.equal(artists);
+        });
+
+        it('peeked artist is not equal the poll artist', () => {
+            const artists = ["Darude", "Hans", "Jackson Wang", "G-Dragon"];
+            const expected = 'Darude';
+
+            artists.forEach(artist => queue.add(artist));
+            queue.poll();
+            const result = queue.peek();
+
+            expect(result).to.not.equal(expected);
+        });
+    });
 })
